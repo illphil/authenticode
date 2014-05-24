d5969aaf4538c2a63918d4ea9b4d7b01c09bc210
illphil <phil.piotrowski@gmail.com>
2014-05-24 19:58:24 +1000

    Fixed to build in windows

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
+import os
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
+libs = ["crypto"]
+if os.name == "nt":
+    libs.append("gdi32")
 
 setup(
     version='0.1',
     description='PE Authenticode verification package',
     name='authenticode',
     author='illphil',
-    email_address='phil@ruxcon.org.au',
-    install_requires=[ 'pefile' ],
-    packages=['authenticode'],
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension(
         "authenticode._authenticode",
-        libraries=["crypto"],
+        libraries=libs,
         sources=["src/authenticode.pyx", "src/osslsigncode.c", "src/osslsigncode_ex.c"])
-    ]
+    ],
 )
